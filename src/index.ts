5b92d0f12fafa4b1765cac77c356d7e31402a483
Antoni Kepinski <a@kepinski.me>
2020-06-11 19:56:10 +0200

    do not use throttle

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import {format} from 'date-fns';
-import pThrottle from 'p-throttle';
 
 import {byLocation} from './utils/get-time-zone';
 
-export default (key: string, {limit = 1, interval = 200}: {limit?: number; interval?: number} = {}) => async (expression: string): Promise<string> => {
-	const throttled = pThrottle(byLocation, limit, interval);
-
+export default (key: string) => async (expression: string): Promise<string> => {
 	const expressionArray = expression.split(' ');
 
 	if (/time|now|pm|am/i.exec(expression)) {
@@ -14,11 +11,11 @@ export default (key: string, {limit = 1, interval = 200}: {limit?: number; inter
 		}
 
 		if (/time/i.exec(expressionArray[0]) && expressionArray[1] === 'in' && expressionArray[2]) {
-			return throttled(key, expressionArray.slice(2).join(' '));
+			return byLocation(key, expressionArray.slice(2).join(' '));
 		}
 
 		if (expressionArray[0] !== expressionArray[0].toUpperCase() && /time/i.exec(expressionArray.slice(-1)[0])) {
-			return throttled(key, expressionArray.slice(0, -1).join(' '));
+			return byLocation(key, expressionArray.slice(0, -1).join(' '));
 		}
 	}
 
